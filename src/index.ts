919ce0c5f22229163a6dbfc6b5fd26af0fc79af2
626259 <72498181+626259@users.noreply.github.com>
2024-01-16 12:42:29 +0600

    project files restructured

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import express, { Request, Response } from "express";
+import express, { Express, Request, Response } from "express";
 import { executeSampleJobs } from "./example";
 
-const app = express();
+const app: Express = express();
 const port = 7000;
 
 app.get("/", async (req: Request, res: Response) => {
